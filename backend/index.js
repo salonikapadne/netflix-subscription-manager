33662d5059a5d76f2513bd4c469595448c0f6fac
salonikapadne <salonikapadne@gmail.com>
2026-04-14 10:55:42 +0530

    Update backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,11 @@ const api = require('./src/routes');
 app.use('/api', api);
 
 app.get('/', (req,res)=> res.send({ok:true, msg:'Netflix subscription API'}));
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 app.listen(port, ()=> console.log('Server running on port', port));
